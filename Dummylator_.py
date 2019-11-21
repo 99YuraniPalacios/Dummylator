4d9728ca0ef2ea2af289524a895a2ad72fa12ffd
Yurani Palacios <yumepa.mp@gmail.com>
2019-11-20 22:36:42 -0500

    Funciones principales

---
+++ Dummylator_.py
@@ -0,0 +1,207 @@
+"""
+@author: ympalacios
+"""
+
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+import numpy as np
+
+class Dummylator(object):
+    
+    def __init__(self):
+        
+        self.valorActual = 0 
+        self.valorGuardado = 0 
+        self.operacionActual = '' 
+        self.positivo = True
+        self.siguienteDecimal = False
+        self.decimales = 0
+        
+        self.textoEntrada = tk.StringVar()
+        self.textoEntrada.set('0')
+    
+    def funcNumero(self,valor):
+        if self.positivo:
+            if self.siguienteDecimal:
+                self.decimales += 1
+                self.valorActual = self.valorActual + (valor * (10**-self.decimales))
+            else:
+                self.valorActual = (self.valorActual * 10) + valor
+        else:
+            if self.siguienteDecimal:
+                self.decimales += 1
+                self.valorActual= self.valorActual - (valor * (10**-self.decimales))
+            else:
+                self.valorActual = (self.valorActual * 10) - valor
+        self.textoEntrada.set(self.valorActual)
+
+    def operacionCambioSigno(self):
+        self.positivo = not self.positivo
+        self.valorActual *= -1
+        self.textoEntrada.set(self.valorActual)
+
+    def reiniciarInfo(self):            
+        self.valorActual = 0
+        self.positivo = True
+        self.siguienteDecimal = False
+        self.decimales = 0
+                
+    def funcOperacionSimple(self,operacion):
+        if operacion == 'Decimal':
+            self.siguienteDecimal = True
+        else:
+            if operacion == 'CambioSigno':
+                self.operacionCambioSigno()
+                
+            elif operacion == 'Igual':
+                if self.operacionActual == 'Suma':
+                    self.valorActual += self.valorGuardado
+                    
+                elif self.operacionActual == 'Resta':
+                    self.valorActual = self.valorGuardado - self.valorActual
+                    
+                elif self.operacionActual == 'Multipl':
+                    self.valorActual *= self.valorGuardado
+                    
+                elif self.operacionActual == 'Division':
+                    self.valorActual = self.valorGuardado / self.valorActual
+                    
+                elif self.operacionActual == 'Sen':
+                    self.valorActual = np.sin(self.valorGuardado)
+                    
+                elif self.operacionActual == 'Senh':
+                    self.valorActual = np.sinh(self.valorGuardado)
+                    
+                elif self.operacionActual == 'Cos':
+                    self.valorActual = np.cos(self.valorGuardado)
+                    
+                elif self.operacionActual == 'Cosh':
+                    self.valorActual = np.cosh(self.valorGuardado)
+                    
+                elif self.operacionActual == 'Tan':
+                    self.valorActual = np.tan(self.valorGuardado)
+                
+                elif self.operacionActual == 'Tanh':
+                    self.valorActual = np.tanh(self.valorGuardado)
+                    
+                elif self.operacionActual == 'Factorial':
+                    self.valorActual = np.math.factorial(self.valorActual)   
+                    
+                elif self.operacionActual == 'Exp':
+                    self.valorActual = (self.valorGuardado)**(self.valorGuardado)
+                    
+                elif self.operacionActual == 'sqrt':
+                    self.valorActual = np.math.sqrt(self.valorGuardado)
+                    
+                elif self.operacionActual == 'log':
+                    self.valorActual = np.log(self.valorGuardado)
+                    
+                elif self.operacionActual == '^':
+                    self.valorActual = pow(self.valorGuardado, self.valorActual)
+                
+                self.valorGuardado = 0
+                self.textoEntrada.set(self.valorActual)
+                self.reiniciarInfo()
+                
+            elif operacion in ["Suma","Resta","Multipl","Division", "Sen","Senh","Cos","Cosh","Tan","Tanh","Factorial","Exp","sqrt","log","^"]:
+                self.valorGuardado = self.valorActual
+                self.operacionActual = operacion
+                self.reiniciarInfo()
+                
+    
+    def funcOperacionFormula(self,operacion):
+        if operacion == 'Decimal':
+            self.siguienteDecimal = True
+        else:
+            if operacion == 'CambioSigno':
+                self.operacionCambioSigno()
+                
+            elif operacion == 'Igual':
+                if self.operacionActual == 'Suma':
+                    self.valorActual += self.valorGuardado
+                    
+                elif self.operacionActual == 'Resta':
+                    self.valorActual = self.valorGuardado - self.valorActual
+                    
+                elif self.operacionActual == 'Multipl':
+                    self.valorActual *= self.valorGuardado
+                    
+                elif self.operacionActual == 'Division':
+                    self.valorActual = self.valorGuardado / self.valorActual
+                    
+                elif self.operacionActual == 'Sen':
+                    self.valorActual = np.sin(self.valorActual)
+                    
+                elif self.operacionActual == 'Senh':
+                    self.valorActual = np.sinh(self.valorActual)
+                    
+                elif self.operacionActual == 'Cos':
+                    self.valorActual = np.cos(self.valorActual)
+                    
+                elif self.operacionActual == 'Cosh':
+                    self.valorActual = np.cosh(self.valorActual)
+                    
+                elif self.operacionActual == 'Tan':
+                    self.valorActual = np.tan(self.valorActual)
+                
+                elif self.operacionActual == 'Tanh':
+                    self.valorActual = np.tanh(self.valorActual)
+                    
+                elif self.operacionActual == 'Factorial':
+                    self.valorActual = np.math.factorial(self.valorGuardado)   
+                    
+                elif self.operacionActual == 'Exp':
+                    self.valorActual = (self.valorGuardado)**(self.valorGuardado)
+                    
+                elif self.operacionActual == 'sqrt':
+                    self.valorActual = np.math.sqrt(self.valorActual)
+                    
+                elif self.operacionActual == 'log':
+                    self.valorActual = np.log(self.valorActual)
+                    
+                elif self.operacionActual == '^':
+                    self.valorActual = pow(self.valorGuardado, self.valorActual)
+                
+                self.valorGuardado = 0
+                self.textoEntrada.set(self.valorActual)
+                self.reiniciarInfo()
+                
+            elif operacion in ["Suma","Resta","Multipl","Division", "Sen","Senh","Cos","Cosh","Tan","Tanh","Factorial","Exp","sqrt","log","^"]:
+                self.valorGuardado = self.valorActual
+                self.operacionActual = operacion
+                self.reiniciarInfo()
+                
+    def modoEntrada(self,ventana,opciones):
+        self.opc = tk.StringVar()
+ 
+        combobox = ttk.Combobox(ventana,font=('Comic Sans MS',8,"bold italic"),textvariable=self.opc)
+        combobox.config(values=opciones)
+        combobox["font"] = ('Comic Sans MS', 8, 'bold italic')
+        combobox.set("Modo de Entrada")
+        combobox.pack()
+        combobox.place(x=320,y=10)
+        combobox.bind("<<ComboboxSelected>>", self.seleccionEntrada)
+
+    def seleccionEntrada(self,event):
+        select=self.opc.get()
+        if select == "Simple":
+            self.funcOperacion = self.funcOperacionSimple
+            messagebox.showinfo(message="El modo de entrada es'Simple': En este modo primero debe digitar el número y después la función", 
+                                title="Modo de Entrada")
+            
+        elif select == "Formula":
+            self.funcOperacion = self.funcOperacionFormula
+            messagebox.showinfo(message="El modo de entrada es 'Formula': En este modo primero debe digitar la función y después el número", 
+                                title="Modo de Entrada")
+        
+    def funcBorrar(self):
+        self.valorGuardado = 0
+        self.textoEntrada.set(self.valorActual)
+        self.reiniciarInfo()
+        
+    def Historial(self):
+        self.f = open('Historial.txt','w')
+        self.f.write(self.valorActual)
+        self.f.close()
+        
